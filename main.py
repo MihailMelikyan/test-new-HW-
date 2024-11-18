4e406a608f6e64584267210a738d6ac8710bcb06
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-11-18 12:53:19 +0300

    ok

---
+++ main.py
@@ -0,0 +1,25 @@
+#1st program
+
+print(9**0.5 * 5 )
+
+#2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+
+#4th program
+
+str = '123.456'
+flt =(float(str) * 10)
+print(int(flt) % 10)
+
+
+
+
+
+
